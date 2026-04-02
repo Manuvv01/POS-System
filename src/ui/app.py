2308e7a75c805f0b3b1d9315c0451b835756c03d
Manuel Vela <manuvvela@gmail.com>
2026-04-02 14:32:41 -0500

    Update app.py

--- src/ui/app.py
+++ src/ui/app.py
@@ -33,13 +33,25 @@ def run_app():
     buttons_frame = tk.Frame(root)
     buttons_frame.grid(row=0, column=2, columnspan=3, sticky="nw", padx=10, pady=10)
 
-    add_button = tk.Button(buttons_frame, text="Agregar", width=15, command=do_nothing)
+    add_button = tk.Button(buttons_frame,
+                           text="Agregar",
+                           width=15,
+                           command=do_nothing)
+
     add_button.grid(row=0, column=0, padx=5)
 
-    search_button = tk.Button(buttons_frame, text="Buscar", width=15, command=do_nothing)
+    search_button = tk.Button(buttons_frame,
+                              text="Buscar",
+                              width=15,
+                              command=do_nothing)
+
     search_button.grid(row=0, column=1, padx=5)
 
-    delete_button = tk.Button(buttons_frame, text="Borrar", width=15, command=do_nothing)
+    delete_button = tk.Button(buttons_frame,
+                              text="Borrar",
+                              width=15,
+                              command=do_nothing)
+
     delete_button.grid(row=0, column=2, padx=5)
 
     # Total Price section
@@ -50,14 +62,6 @@ def run_app():
                               font=("Courier New", 20), state="disabled")
     total_price_box.grid(row=2, column=0, columnspan=3, sticky="w", pady=(0, 10))
 
-    # Money input
-    tk.Label(buttons_frame, text="Ingrese Efectivo", font=("Arial", 20)) \
-        .grid(row=3, column=0, columnspan=3, sticky="w")
-
-    money_box = tk.Text(buttons_frame, height=1, width=30,
-                        font=("Courier New", 20))
-    money_box.grid(row=4, column=0, columnspan=3, sticky="w", pady=(0, 10))
-
     # Change section
     tk.Label(buttons_frame, text="Cambio", font=("Arial", 20)) \
         .grid(row=5, column=0, columnspan=3, sticky="w")
@@ -65,5 +69,15 @@ def run_app():
     change_box = tk.Text(buttons_frame, height=3, width=30,
                          font=("Courier New", 20), state="disabled")
     change_box.grid(row=6, column=0, columnspan=3, sticky="w")
+
+    pay_button = tk.Button(
+        buttons_frame,
+        text= "Pagar",
+        font= ("Arial", 16),
+        width= 15,
+        command= do_nothing
+    )
+
+    pay_button.grid(row=7, column=0, columnspan=3, pady=15)
 
     root.mainloop()
