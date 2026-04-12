6d6a4c80107902464d49e1386a5346a685a3096d
Manuel Vela <manuvvela@gmail.com>
2026-04-11 21:53:43 -0500

    Create a center window utility

--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -15,3 +15,15 @@ def ensure_file_exists(path: str):
     """
     if not os.path.exists(path):
         raise FileNotFoundError(f"File not found: {path}")
+
+
+def center_window(parent, window):
+    window.update_idletasks()
+
+    width = window.winfo_width()
+    height = window.winfo_height()
+
+    x = parent.winfo_x() + (parent.winfo_width() // 2) - (width // 2)
+    y = parent.winfo_y() + (parent.winfo_height() // 2) - (height // 2)
+
+    window.geometry(f"{width}x{height}+{x}+{y}")
